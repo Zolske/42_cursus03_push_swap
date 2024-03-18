3cb29f37b264ee446d7915294f28ed9feac2f725
Zoltan Kepes <zkepes@c2r5s8.42london.com>
2024-03-18 18:20:44 +0000

    add get_next_line

---
+++ 03_cursus/pipex/libft/get_next_line.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkepes <zkepes@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 10:53:54 by zkepes            #+#    #+#             */
+/*   Updated: 2023/12/08 11:48:19 by zkepes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+->	Linux systems limit the number of file descriptors that any one process
+	may	open to 1024 per process. (0 - 1023)
+*/
+
+#include <unistd.h>
+#include <stdbool.h>
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	int			idx;
+	int			read_status;
+	static char	*array[1028] = {NULL};
+
+	idx = ft_malloc_set_idx(&array[fd], 0, false);
+	while (idx == NOT_FOUND_N)
+	{
+		ft_malloc_set_idx(&array[STR_SWAP], BUFFER_SIZE + 1, true);
+		read_status = read(fd, array[STR_SWAP], BUFFER_SIZE);
+		if (read_status <= READ_ERROR)
+			return (ft_free_null(&(array[fd]), &(array[N]),
+					&(array[STR_SWAP])));
+		if (read_status == END_OF_FILE && (array[fd] == NULL))
+			return (ft_free_null(&(array[STR_SWAP]), &(array[N]), &(array[N])));
+		if (read_status == END_OF_FILE && (array[fd] != NULL))
+		{
+			array[STR_RETURN] = array[fd];
+			array[fd] = NULL;
+			ft_free_null(&(array[STR_SWAP]), &(array[N]), &(array[N]));
+			return (array[STR_RETURN]);
+		}
+		ft_add_read(array, fd);
+		idx = ft_malloc_set_idx(&array[fd], 0, false);
+	}
+	return (ft_split_read(array, fd, idx));
+}
---
+++ 03_cursus/pipex/libft/get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkepes <zkepes@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 10:54:50 by zkepes            #+#    #+#             */
+/*   Updated: 2023/11/20 11:16:47 by zkepes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <stdbool.h>
+# define STR_RETURN 1025
+# define STR_SWAP 1026
+# define N 1027
+# define READ_ERROR -1
+# define END_OF_FILE 0
+# define NOT_FOUND_N -1
+
+int		ft_malloc_set_idx(char **ptr, size_t size, bool mal);
+char	*get_next_line(int fd);
+char	*ft_split_read(char *array[], int fd, int idx);
+char	*ft_free_null(char **ptr1, char **ptr2, char **ptr3);
+void	ft_scpy(char *dst, char *src, int len);
+void	ft_add_read(char *array[], int fd);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000000
+# endif
+
+#endif
