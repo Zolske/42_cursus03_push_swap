f30aec494b9502f25c67df7c2823e72e760ddb93
Zoltan Kepes <zkepes@c2r5s8.42london.com>
2024-04-15 19:05:21 +0100

    need to fix errors

--- 03_cursus/solong/src/init_img.c
+++ 03_cursus/solong/src/init_img.c
@@ -6,7 +6,7 @@
 /*   By: zkepes <zkepes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 10:44:12 by zkepes            #+#    #+#             */
-/*   Updated: 2024/04/14 19:07:17 by zkepes           ###   ########.fr       */
+/*   Updated: 2024/04/15 18:49:59 by zkepes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ void	init_images(t_data *d)
 
 void	init_static(t_data *d)
 {
-	d->img.text.arrow_key = new_file_img(d, "img/text/keys2.xpm");
-	d->img.text.won = new_file_img(d, "img/text/won.xpm");
-	d->img.text.lost = new_file_img(d, "img/text/lost.xpm");
-	d->img.i_sta.wall = new_file_img(d, "img/stonebrickbluefull.xpm");
-	d->img.i_sta.sand = new_file_img(d, "img/sandfull.xpm");
-	d->img.i_sta.wallh = new_file_img(d, "img/stonebrickblue.xpm");
+	d->img.text.arrow_key = new_file_img(d, "textures/text/keys2.xpm");
+	d->img.text.won = new_file_img(d, "textures/text/won.xpm");
+	d->img.text.lost = new_file_img(d, "textures/text/lost.xpm");
+	d->img.i_sta.wall = new_file_img(d, "textures/stonebrickbluefull.xpm");
+	d->img.i_sta.sand = new_file_img(d, "textures/sandfull.xpm");
+	d->img.i_sta.wallh = new_file_img(d, "textures/stonebrickblue.xpm");
 }
 
 void	init_img_player(t_data *d)
@@ -46,32 +46,32 @@ void	init_img_player(t_data *d)
 
 void	init_img_coin(t_data *d)
 {
-	d->img.coin[0] = new_file_img(d, "img/coin/coin_00.xpm");
-	d->img.coin[1] = new_file_img(d, "img/coin/coin_00.xpm");
-	d->img.coin[2] = new_file_img(d, "img/coin/coin_01.xpm");
-	d->img.coin[3] = new_file_img(d, "img/coin/coin_02.xpm");
-	d->img.coin[4] = new_file_img(d, "img/coin/coin_03.xpm");
-	d->img.coin[5] = new_file_img(d, "img/coin/coin_04.xpm");
-	d->img.coin[6] = new_file_img(d, "img/coin/coin_04.xpm");
-	d->img.coin[7] = new_file_img(d, "img/coin/coin_05.xpm");
-	d->img.coin[8] = new_file_img(d, "img/coin/coin_06.xpm");
-	d->img.coin[9] = new_file_img(d, "img/coin/coin_07.xpm");
-	d->img.coin[10] = new_file_img(d, "img/coin/coin_08.xpm");
-	d->img.coin[11] = new_file_img(d, "img/coin/coin_09.xpm");
+	d->img.coin[0] = new_file_img(d, "textures/coin/coin_00.xpm");
+	d->img.coin[1] = new_file_img(d, "textures/coin/coin_00.xpm");
+	d->img.coin[2] = new_file_img(d, "textures/coin/coin_01.xpm");
+	d->img.coin[3] = new_file_img(d, "textures/coin/coin_02.xpm");
+	d->img.coin[4] = new_file_img(d, "textures/coin/coin_03.xpm");
+	d->img.coin[5] = new_file_img(d, "textures/coin/coin_04.xpm");
+	d->img.coin[6] = new_file_img(d, "textures/coin/coin_04.xpm");
+	d->img.coin[7] = new_file_img(d, "textures/coin/coin_05.xpm");
+	d->img.coin[8] = new_file_img(d, "textures/coin/coin_06.xpm");
+	d->img.coin[9] = new_file_img(d, "textures/coin/coin_07.xpm");
+	d->img.coin[10] = new_file_img(d, "textures/coin/coin_08.xpm");
+	d->img.coin[11] = new_file_img(d, "textures/coin/coin_09.xpm");
 }
 
 void	init_img_exit(t_data *d)
 {
-	d->img.exit[0] = new_file_img(d, "img/exit/exit_00.xpm");
-	d->img.exit[1] = new_file_img(d, "img/exit/exit_01.xpm");
-	d->img.exit[2] = new_file_img(d, "img/exit/exit_02.xpm");
-	d->img.exit[3] = new_file_img(d, "img/exit/exit_03.xpm");
-	d->img.exit[4] = new_file_img(d, "img/exit/exit_04.xpm");
-	d->img.exit[5] = new_file_img(d, "img/exit/exit_05.xpm");
-	d->img.exit[6] = new_file_img(d, "img/exit/exit_00.xpm");
-	d->img.exit[7] = new_file_img(d, "img/exit/exit_01.xpm");
-	d->img.exit[8] = new_file_img(d, "img/exit/exit_02.xpm");
-	d->img.exit[9] = new_file_img(d, "img/exit/exit_03.xpm");
-	d->img.exit[10] = new_file_img(d, "img/exit/exit_04.xpm");
-	d->img.exit[11] = new_file_img(d, "img/exit/exit_05.xpm");
+	d->img.exit[0] = new_file_img(d, "textures/exit/exit_00.xpm");
+	d->img.exit[1] = new_file_img(d, "textures/exit/exit_01.xpm");
+	d->img.exit[2] = new_file_img(d, "textures/exit/exit_02.xpm");
+	d->img.exit[3] = new_file_img(d, "textures/exit/exit_03.xpm");
+	d->img.exit[4] = new_file_img(d, "textures/exit/exit_04.xpm");
+	d->img.exit[5] = new_file_img(d, "textures/exit/exit_05.xpm");
+	d->img.exit[6] = new_file_img(d, "textures/exit/exit_00.xpm");
+	d->img.exit[7] = new_file_img(d, "textures/exit/exit_01.xpm");
+	d->img.exit[8] = new_file_img(d, "textures/exit/exit_02.xpm");
+	d->img.exit[9] = new_file_img(d, "textures/exit/exit_03.xpm");
+	d->img.exit[10] = new_file_img(d, "textures/exit/exit_04.xpm");
+	d->img.exit[11] = new_file_img(d, "textures/exit/exit_05.xpm");
 }
--- 03_cursus/solong/src/init_img_num.c
+++ 03_cursus/solong/src/init_img_num.c
@@ -6,7 +6,7 @@
 /*   By: zkepes <zkepes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 15:46:32 by zkepes            #+#    #+#             */
-/*   Updated: 2024/04/14 15:58:22 by zkepes           ###   ########.fr       */
+/*   Updated: 2024/04/15 18:17:15 by zkepes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	init_img_num(t_data *d)
 {
-	d->img.text.moves = new_file_img(d, "img/text/moves.xpm");
-	d->img.text.num[0] = new_file_img(d, "img/text/0.xpm");
-	d->img.text.num[1] = new_file_img(d, "img/text/1.xpm");
-	d->img.text.num[2] = new_file_img(d, "img/text/2.xpm");
-	d->img.text.num[3] = new_file_img(d, "img/text/3.xpm");
-	d->img.text.num[4] = new_file_img(d, "img/text/4.xpm");
-	d->img.text.num[5] = new_file_img(d, "img/text/5.xpm");
-	d->img.text.num[6] = new_file_img(d, "img/text/6.xpm");
-	d->img.text.num[7] = new_file_img(d, "img/text/7.xpm");
-	d->img.text.num[8] = new_file_img(d, "img/text/8.xpm");
-	d->img.text.num[9] = new_file_img(d, "img/text/9.xpm");
+	d->img.text.moves = new_file_img(d, "textures/text/moves.xpm");
+	d->img.text.num[0] = new_file_img(d, "textures/text/0.xpm");
+	d->img.text.num[1] = new_file_img(d, "textures/text/1.xpm");
+	d->img.text.num[2] = new_file_img(d, "textures/text/2.xpm");
+	d->img.text.num[3] = new_file_img(d, "textures/text/3.xpm");
+	d->img.text.num[4] = new_file_img(d, "textures/text/4.xpm");
+	d->img.text.num[5] = new_file_img(d, "textures/text/5.xpm");
+	d->img.text.num[6] = new_file_img(d, "textures/text/6.xpm");
+	d->img.text.num[7] = new_file_img(d, "textures/text/7.xpm");
+	d->img.text.num[8] = new_file_img(d, "textures/text/8.xpm");
+	d->img.text.num[9] = new_file_img(d, "textures/text/9.xpm");
 }
